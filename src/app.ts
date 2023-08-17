09b0318ebe6311f74ac82757fb0b63c8236d60c4
unknown <md.asif.8393@gmail.com>
2023-08-17 11:32:05 +0600

    Done: setup express, cors

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,16 @@
 import express, { Application, Request, Response } from 'express';
-const app:Application = express()
-const port = 3000
+import cors from 'cors';
+
+const app:Application = express();
+
+app.use(cors());
+
+//parser
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 app.get('/', (req:Request, res:Response) => {
-  res.send('Hello World!')
+  res.send('Working successfully!')
 })
 
 export default app;
