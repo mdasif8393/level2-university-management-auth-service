ad153aa6a357a1b05df5b989bbfc96c6fd679ae3
unknown <md.asif.8393@gmail.com>
2023-08-20 19:37:50 +0600

    change

--- src/server.ts
+++ src/server.ts
@@ -1,18 +1,19 @@
-import mongoose from 'mongoose';
-import app from './app';
-import config from './config/index';
+import mongoose from 'mongoose'
+import app from './app'
+import config from './config/index'
+import logger from './shared/logger'
 
 async function bootstrap() {
   try {
-    await mongoose.connect(config.database_url as string);
-    console.log('Database is connected successfully');
+    await mongoose.connect(config.database_url as string)
+    logger.info('Database is connected successfully')
 
     app.listen(config.port, () => {
-      console.log(`Application listening on port ${config.port}`);
-    });
+      logger.info(`Application listening on port ${config.port}`)
+    })
   } catch (err) {
-    console.log('Failed to connect database', err);
+    logger.error('Failed to connect database', err)
   }
 }
 
-bootstrap();
+bootstrap()
