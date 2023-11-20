75dc7cb84453ae975cf419b1a4d106a46609e2cf
unknown <md.asif.8393@gmail.com>
2023-11-20 10:11:06 +0600

    [Done -> Faculty costant]

---
+++ src/app/modules/faculty/faculty.constant.ts
@@ -0,0 +1,19 @@
+export const facultyFilterableFields = [
+  'searchTerm',
+  'id',
+  'gender',
+  'bloodGroup',
+  'email',
+  'contactNo',
+  'emergencyContactNo',
+  'designation',
+];
+
+export const facultySearchableFields = [
+  'email',
+  'contactNo',
+  'emergencyContactNo',
+  'name.firstName',
+  'name.lastName',
+  'name.middleName',
+];
