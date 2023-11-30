3ca2c6e02acacbd4b1f9347658dd9d33738c41f4
unknown <md.asif.8393@gmail.com>
2023-11-30 20:08:15 +0600

    [Done]

--- src/app/modules/academicFaculty/academicFaculty.route.ts
+++ src/app/modules/academicFaculty/academicFaculty.route.ts
@@ -20,7 +20,6 @@ router.get(
     ENUM_USER_ROLE.SUPER_ADMIN,
     ENUM_USER_ROLE.ADMIN,
     ENUM_USER_ROLE.FACULTY,
-    ENUM_USER_ROLE.STUDENT,
   ),
   AcademicFacultyController.getSingleFaculty,
 );
@@ -37,17 +36,17 @@ router.patch(
 
 router.delete(
   '/:id',
-  auth(ENUM_USER_ROLE.SUPER_ADMIN, ENUM_USER_ROLE.ADMIN),
+  auth(ENUM_USER_ROLE.SUPER_ADMIN),
   AcademicFacultyController.deleteFaculty,
 );
 
 router.get(
   '/',
-  auth(
-    ENUM_USER_ROLE.SUPER_ADMIN,
-    ENUM_USER_ROLE.ADMIN,
-    ENUM_USER_ROLE.STUDENT,
-  ),
+  // auth(
+  //   ENUM_USER_ROLE.SUPER_ADMIN,
+  //   ENUM_USER_ROLE.ADMIN,
+  //   ENUM_USER_ROLE.STUDENT,
+  // ),
   AcademicFacultyController.getAllFaculties,
 );
 
--- src/app/modules/academicSemester/academicSemester.route.ts
+++ src/app/modules/academicSemester/academicSemester.route.ts
@@ -4,24 +4,50 @@ import validateRequest from '../../middlewares/validateRequest';
 
 import { AcademicSemesterController } from './academicSemester.controller';
 import { AcademicSemesterValidation } from './acdemicSemester.validation';
+import auth from '../../middlewares/auth';
+import { ENUM_USER_ROLE } from '../../../enums/user';
 const router = express.Router();
 
 router.post(
   '/create-semester',
   validateRequest(AcademicSemesterValidation.createAcademicSemesterZodSchema),
+  auth(ENUM_USER_ROLE.SUPER_ADMIN, ENUM_USER_ROLE.ADMIN),
   AcademicSemesterController.createSemester,
 );
 
-router.get('/:id', AcademicSemesterController.getSingleSemester);
+router.get(
+  '/:id',
+  auth(
+    ENUM_USER_ROLE.SUPER_ADMIN,
+    ENUM_USER_ROLE.ADMIN,
+    ENUM_USER_ROLE.FACULTY,
+    ENUM_USER_ROLE.STUDENT,
+  ),
+  AcademicSemesterController.getSingleSemester,
+);
 
 router.patch(
   '/:id',
   validateRequest(AcademicSemesterValidation.updateAcademicSemesterZodSchema),
+  auth(ENUM_USER_ROLE.SUPER_ADMIN, ENUM_USER_ROLE.ADMIN),
   AcademicSemesterController.updateSemester,
 );
 
-router.delete('/:id', AcademicSemesterController.deleteSemester);
+router.delete(
+  '/:id',
+  auth(ENUM_USER_ROLE.SUPER_ADMIN, ENUM_USER_ROLE.ADMIN),
+  AcademicSemesterController.deleteSemester,
+);
 
-router.get('/', AcademicSemesterController.getAllSemesters);
+router.get(
+  '/',
+  // auth(
+  //   ENUM_USER_ROLE.SUPER_ADMIN,
+  //   ENUM_USER_ROLE.ADMIN,
+  //   ENUM_USER_ROLE.FACULTY,
+  //   ENUM_USER_ROLE.STUDENT
+  // )
+  AcademicSemesterController.getAllSemesters,
+);
 
 export const AcademicSemesterRoutes = router;
